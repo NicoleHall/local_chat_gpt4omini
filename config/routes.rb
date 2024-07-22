ee1c69f2a15706aa8ed15aabeae0c8425e912d77
Nicole Hall <nicolealexandrahall@gmail.com>
2024-07-22 12:03:28 -0600

    Fix typo in routes: change chat_reponses to chat_responses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "chats#index"
-  resource :chat_reponses, only: [:show]
+  resource :chat_responses, only: [:show]
 end
